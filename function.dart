223cfd8b6564c53cb4d63f2b17b5e54f3dfc03be
ekaimenyi_ <eliaskaimenyi@gmail.com>
2024-04-10 13:55:20 +0300

    first commit

---
+++ function.dart
@@ -0,0 +1,13 @@
+int sum(int num1, int num2) {
+  //function i have created
+  int answer = num1 + num2;
+  return answer;
+}
+
+void main() {
+  //main function
+  int num1 = 20; //declaring the variables
+  int num2 = 30;
+  int result = sum(num1, num2);
+  print(result); //printing the output
+}
